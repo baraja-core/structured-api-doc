59e3fa76ced60da5d1ca45ef57c84c40c8481628
Jan Barášek <janbarasek@gmail.com>
2022-07-30 15:50:12 +0200

    React: Recompile new structure.

---
+++ out/_next/static/AC1lmbU-s_WaNqUOi-FtB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/105-1e21daa61467e1a8.js","static/chunks/pages/index-2ad6b59e3af4f14c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
